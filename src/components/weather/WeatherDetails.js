42635eecb6cb3aecb7dbc19ada94bc64b7389100
ERkrishnendu <krishnendu.rantu@gmail.com>
2022-04-10 23:20:59 +0530

    removed package.lock

--- src/components/weather/WeatherDetails.js
+++ src/components/weather/WeatherDetails.js
@@ -7,16 +7,20 @@ const WeatherDetails = (props) => {
   return (
     <div className='weahter_info'>
           <div className='city_name'>
-            City Name: {weather?.name}
+            {weather?.name}
           </div>
           <div className='city_country'>
-          Country: {weather?.sys?.country} 
+           {weather?.sys?.country} 
           </div>
           <div className='city_temp'>
-          Temperature : {Math.round(weather?.main?.temp - 273.15)} ° C
+          
+          <img src={`http://openweathermap.org/img/wn/${weather?.weather[0]?.icon}.png`} />
+          <span>{Math.round(weather?.main?.temp - 273.15)} ° C</span>
           </div>
           <div className='city_feels_like'>
-          Feel : {Math.round(weather?.main?.feels_like - 273.15)} ° C
+          <span>Feel</span>  
+          <span>{Math.round(weather?.main?.feels_like - 273.15)} ° C</span>
+          <span>{weather?.weather[0]?.main}</span>
           </div>
           <div className='city_humidity'>
           humidity : {weather?.main?.humidity}%
--- src/components/weather/WeatherMain.js
+++ src/components/weather/WeatherMain.js
@@ -6,8 +6,11 @@ import axios from 'axios';
 const WeatherMain = () => {
     
 /* for instant search with on submit */
-const [city, updateCity] = useState(null);   
-const [weather,updateWeather] = useState();
+
+
+
+const [city, updateCity] = useState('');   
+const [weather,updateWeather] = useState('');
 
 
 /* for instant search with on submit */
@@ -15,7 +18,7 @@ const fetchWeather = async (e) => {
     e.preventDefault();
     const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fec40eb498208bde1cbf13a788a62e34`);
     updateWeather(response.data);
-    //console.log(response.data)
+    console.log(response.data)
 }
 
 
